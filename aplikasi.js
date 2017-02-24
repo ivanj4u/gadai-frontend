adf22dde37c6a0686365452c171f520df8c5c27a
Ivan Aribanilia <angko.j4u@gmail.com>
2017-02-24 10:58:07 +0700

    Update proxy utk heroku

--- aplikasi.js
+++ aplikasi.js
@@ -4,19 +4,19 @@
 var express  = require('express');
 var app      = express();
 var httpProxy = require('http-proxy');
-var apiProxy = httpProxy.createProxyServer();
 var authServer = 'https://gd-server.herokuapp.com/',
   backendServer = 'https://gd-backend.herokuapp.com/';
+var routing = {
+  '/oauth/**': { port: port, host: authServer },
+  '/api/**': { port: port, host: backendServer }
+}
+var apiProxy = httpProxy.createProxyServer(routing);
 
-app.all("/oauth/*", function(req, res) {
+app.all("/oauth/*", function (req, res) {
   console.log('redirecting to auth server');
   apiProxy.web(req, res, {target: authServer});
 });
 
-app.all("/api/*", function(req, res) {
-  console.log('redirecting to backend server');
-  apiProxy.web(req, res, {target: backendServer});
-});
 app.use(express.static(__dirname + '/dist'));
 console.log("Aplikasi sudah dijalankan");
 app.listen(process.env.PORT || 3000);
